3683864c958122e7d1c3653d046ffb6c665cc358
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-03-19 23:38:14 +0200

    Design the game

--- src/scenes/levelScene.js
+++ src/scenes/levelScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import { state } from '../config/state';
+import { platformer } from './platformer';
 
 export default class Level extends Phaser.Scene {
   constructor() {
@@ -14,6 +15,27 @@ export default class Level extends Phaser.Scene {
 
     state.background.setOrigin(0, 0);
     state.background2.setOrigin(0, 0);
+
+    const gameWidth = parseFloat(state.background.getBounds().width);
+    state.width = gameWidth;
+    const windowWidth = 800;
+
+    const bd1Width = state.background.getBounds().width;
+    state.background.setScrollFactor(
+      (bd1Width - windowWidth) / (gameWidth - windowWidth),
+    );
+
+    state.scoreText = this.add.text(16, 16, `score: ${state.score}`, {
+      fontSize: '40px',
+      fill: '#ff0000',
+    });
+    state.player = this.physics.add.sprite(80, 110, 'dude').setScale(1);
+    state.platforms = this.physics.add.staticGroup();
+    state.spikes = this.physics.add.staticGroup();
+    state.coin = this.physics.add.staticGroup();
+    state.apple = this.physics.add.staticGroup();
+
+    platformer(this, state);
   }
 
   update() {
