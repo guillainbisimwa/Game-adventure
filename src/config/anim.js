96388d8280decf30540b4da03ead76613d28e9cf
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-03-20 00:03:23 +0200

    Add anim file

---
+++ src/config/anim.js
@@ -0,0 +1,43 @@
+export const anim = (context) => {
+  context.anims.create({
+    key: 'right',
+    frames: context.anims.generateFrameNumbers('dude', { start: 8, end: 11 }),
+    frameRate: 0,
+    repeat: -1,
+  });
+
+  context.anims.create({
+    key: 'left',
+    frames: context.anims.generateFrameNumbers('dude', { start: 4, end: 7 }),
+    frameRate: 0,
+    repeat: -1,
+  });
+
+  context.anims.create({
+    key: 'idle',
+    frames: context.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  context.anims.create({
+    key: 'jump',
+    frames: context.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  context.anims.create({
+    key: 'playbutton',
+    frames: context.anims.generateFrameNumbers('play', { start: 0, end: 2 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  context.anims.create({
+    key: 'snowmanAlert',
+    frames: context.anims.generateFrameNumbers('snowman', { start: 0, end: 3 }),
+    frameRate: 4,
+    repeat: -1,
+  });
+};
