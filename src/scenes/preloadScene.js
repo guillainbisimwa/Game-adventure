0d6897d0d7df2028bd476f30a6558482f34c165e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-03-19 19:19:27 +0200

    Add preload scene

---
+++ src/scenes/preloadScene.js
@@ -0,0 +1,69 @@
+import Phaser from 'phaser';
+
+import dude from '../assets/dude.png';
+import monster from '../assets/monster.png';
+import background from '../assets/Background/Background_01.png';
+
+import ground02 from '../assets/Platformer/Ground_02.png';
+import ground03 from '../assets/Platformer/Ground_03.png';
+import ground04 from '../assets/Platformer/Ground_04.png';
+import ground06 from '../assets/Platformer/Ground_06.png';
+import ground07 from '../assets/Platformer/Ground_07.png';
+import ground08 from '../assets/Platformer/Ground_08.png';
+import ground09 from '../assets/Platformer/Ground_09.png';
+import ground10 from '../assets/Platformer/Ground_10.png';
+import ground11 from '../assets/Platformer/Ground_11.png';
+import ground12 from '../assets/Platformer/Ground_12.png';
+import ground13 from '../assets/Platformer/Ground_13.png';
+import coin from '../assets/Collectable Object/Coin_01.png';
+import apple from '../assets/Collectable Object/Apple.png';
+import bonus from '../assets/Platformer/Bonus.png';
+import brick01 from '../assets/Platformer/Brick_01.png';
+import brick02 from '../assets/Platformer/Brick_02.png';
+import decorBrick from '../assets/Platformer/Decor_Brick.png';
+import spikes from '../assets/Platformer/Spikes.png';
+import door01 from '../assets/Platformer/Door_01.png';
+
+export default class Preload extends Phaser.Scene {
+  constructor() {
+    super('Preload');
+  }
+
+  preload() {
+    this.load.image('ground02', ground02);
+    this.load.image('ground03', ground03);
+    this.load.image('ground04', ground04);
+    this.load.image('ground06', ground06);
+    this.load.image('ground07', ground07);
+    this.load.image('ground08', ground08);
+    this.load.image('ground09', ground09);
+    this.load.image('ground10', ground10);
+    this.load.image('ground11', ground11);
+    this.load.image('ground12', ground12);
+    this.load.image('ground13', ground13);
+    this.load.image('bonus', bonus);
+    this.load.image('brick01', brick01);
+    this.load.image('brick02', brick02);
+    this.load.image('decorBrick', decorBrick);
+    this.load.image('spikes', spikes);
+    this.load.image('door01', door01);
+    this.load.image('coin', coin);
+    this.load.image('apple', apple);
+
+    this.load.spritesheet('dude', dude, {
+      frameWidth: 30,
+      frameHeight: 45,
+    });
+    this.load.image('background', background);
+
+    this.load.spritesheet('monster', monster, {
+      frameWidth: 50,
+      frameHeight: 70,
+    });
+    this.load.on('complete', this.complete, { scene: this.scene });
+  }
+
+  complete() {
+    
+  }
+}
