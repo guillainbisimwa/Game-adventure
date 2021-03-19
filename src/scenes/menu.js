88bd6102b51813efe50ba48999c90b9297de407f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-03-19 22:49:34 +0200

    Add hover effects to buttons

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -8,12 +8,33 @@ export default class Menu extends Phaser.Scene {
 
   create() {
     st.bg = this.add.image(400, 300, 'bg').setScale(0.8);
-    st.optionButton = this.add.image(400, 240, 'play').setScale(0.8);
-    st.optionButton = this.add.image(400, 340, 'scores').setScale(0.8);
-    st.optionButton = this.add.image(400, 440, 'help').setScale(0.8);
-    st.optionButton = this.add.image(400, 540, 'back').setScale(0.8);
+    st.playButton = this.add.image(400, 240, 'play').setScale(0.8);
+    st.scoresButton = this.add.image(400, 340, 'scores').setScale(0.8);
+    st.helpButton = this.add.image(400, 440, 'help').setScale(0.8);
+    st.backButton = this.add.image(400, 540, 'back').setScale(0.8);
+
+    st.playButton.setInteractive({ useHandCursor: true });
+    this.onHover(st.playButton);
+
+    st.scoresButton.setInteractive({ useHandCursor: true });
+    this.onHover(st.scoresButton);
+
+    st.helpButton.setInteractive({ useHandCursor: true });
+    this.onHover(st.helpButton);
+
+    st.backButton.setInteractive({ useHandCursor: true });
+    this.onHover(st.backButton);
   }
 
   update() {
+  }
+
+  onHover(obj) {
+    obj.on('pointerover', () => {
+      obj.setTint(0xecdccc);
+    });
+    obj.on('pointerout', () => {
+      obj.setTint();
+    });
   }
 }
