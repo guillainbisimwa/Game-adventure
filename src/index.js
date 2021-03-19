b2f45903772d7da74465603011ef1da029de408b
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-03-19 18:56:45 +0200

    Add index js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './style.css';
+import Phaser from 'phaser';
+import config from './config/config';
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+  }
+}
+
+window.game = new Game();
