0e553f175c8bab436a8604126c2a7c6f9fecbb81
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-03-25 02:16:38 +0200

    Fetch scores from API and display them

--- src/scenes/bestScores.js
+++ src/scenes/bestScores.js
@@ -11,23 +11,22 @@ export default class BestScores extends Phaser.Scene {
     st.backButton = this.add.image(400, 540, 'back').setScale(0.8);
 
     this.add.text(190, 130, '10 BEST SCORES', { fontSize: '50px', fill: '#000000' });
+    const print = this.add.text(250, 200, '', { fontSize: '25px', fill: '#000000' });
 
-    const list = [
-      { name: 'Guy', score: 50 },
-      { name: 'Eva', score: 70 },
-      { name: 'John', score: 10 },
-      { name: 'Josh', score: 20 },
-      { name: 'Starr', score: 60 },
-      { name: 'Kim', score: 90 },
-      { name: 'Joe', score: 90 },
-      { name: 'jeremy', score: 90 },
-      { name: 'Aline', score: 90 },
-      { name: 'Brack', score: 90 },
-    ];
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://k-backend-api.herokuapp.com/api/scores', true);
 
-    for (let i = 0; i < list.length; i += 1) {
-      this.add.text(250, 200 + (i * 30), `${i + 1}. ${list[i].name}: [${list[i].score}]`, { fontSize: '25px', fill: '#000000' });
-    }
+    xhr.onload = () => {
+      const req = JSON.parse(xhr.responseText);
+      req.sort((a, b) => b.scores - a.scores);
+      let i = 1;
+      req.slice(0, 10).forEach(element => {
+        print.text += `${i}. ${element.name} - [${element.scores}] \n`;
+        i += 1;
+      });
+    };
+
+    xhr.send(null);
 
     st.backButton.setInteractive({ useHandCursor: true });
     this.onHover(st.backButton);
